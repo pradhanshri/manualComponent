323d0df3d9ddb73e6b8d28f6593a361077c93059
pradhanshri <pshripad10@gmail.com>
2023-04-22 16:17:46 +0530

    Manual Component Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { productComponent } from './shared/component/product.component';
+import { productsComponent } from './shared/component/products.component';
+import { aproductsComp } from './shared/component/All Products/aproducts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    productComponent,
+    productsComponent,
+    aproductsComp
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/component/All Products/aproducts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:`app-aproducts`,
+    templateUrl:`./aproducts.component.html`,
+    styleUrls: [`./aproducts.component.scss`]
+})
+
+export class aproductsComp{
+    
+}
